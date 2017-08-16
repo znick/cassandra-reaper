d23663b6664b71de45d325098ee78db339652f58
Radovan Zvoncek <radovan@thelastpickle.com>
2017-08-16 10:38:46 +0300

    Move java opts to reaper env file

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = 'cassandra-reaper' ]; then
     set -x
     su-exec reaper /usr/local/bin/append-persistence.sh
-    exec su-exec reaper java -jar ${JAVA_OPTS} \
+    exec su-exec reaper java ${JAVA_OPTS} -jar \
         /usr/local/lib/cassandra-reaper.jar server /etc/cassandra-reaper.yml
 fi
 
